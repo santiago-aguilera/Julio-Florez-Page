8fc0801159b20ea1ad45b0f42e7e0ec08475bebe
Santiago Aguilera <santiago.aguilera681@gmail.com>
2025-07-01 17:44:39 -0500

    Final de Proyectos

--- Pagina Julio Florez/JS/Buscador.js
+++ Pagina Julio Florez/JS/Buscador.js
@@ -1,9 +1,9 @@
 document.getElementById("buscador").addEventListener("keyup", function () {
-  const texto = this.value.toLowerCase();
-  const libros = document.querySelectorAll(".libro");
+    const texto = this.value.toLowerCase();
+    const libros = document.querySelectorAll(".libro");
 
-  libros.forEach(libro => {
-    const titulo = libro.querySelector("h3").textContent.toLowerCase();
-    libro.style.display = titulo.includes(texto) ? "block" : "none";
-  });
+    libros.forEach(libro => {
+        const titulo = libro.querySelector("h3").textContent.toLowerCase();
+        libro.style.display = titulo.includes(texto) ? "block" : "none";
+    });
 });
