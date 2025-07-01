ca196aafe65001548bc1761061d52f895414abf8
Santiago Aguilera <santiago.aguilera681@gmail.com>
2025-07-01 17:34:14 -0500

    Biblioteca final

---
+++ Pagina Julio Florez/JS/Biblioteca.js
@@ -0,0 +1,8 @@
+function mostrarContenido() {
+                                    var x = document.getElementById("contenidoOculto");
+                                    if (x.style.display === "none") {
+                                        x.style.display = "block";
+                                    } else {
+                                        x.style.display = "none";
+                                    }
+                                }
